6b5c54b013f8522154898d800ef939226a4e2085
idev-al <66803418+idev-al@users.noreply.github.com>
2024-10-25 18:43:25 +0200

    Update idev_chat.py

--- idev_chat.py
+++ idev_chat.py
@@ -3,16 +3,16 @@ import openai
 from llama_index.llms.openai import OpenAI
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Chat with the KB chat, powered by iDev", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
-st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
-st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.title("Chat with the iDev chat docs")
+st.info("web: https://idev.al", icon="📃")
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about Streamlit's open-source Python library!",
+            "content": "Ask me a question about iDev",
         }
     ]
 
@@ -21,14 +21,12 @@ def load_data():
     reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
     docs = reader.load_data()
     Settings.llm = OpenAI(
-        model="gpt-3.5-turbo",
+        model="gpt-4o-mini",
         temperature=0.2,
         system_prompt="""You are an expert on 
-        the Streamlit Python library and your 
-        job is to answer technical questions. 
+        iDev and your job is to answer technical questions. 
         Assume that all questions are related 
-        to the Streamlit Python library. Keep 
-        your answers technical and based on 
+        to iDev. Keep your answers technical and based on 
         facts – do not hallucinate features.""",
     )
     index = VectorStoreIndex.from_documents(docs)
